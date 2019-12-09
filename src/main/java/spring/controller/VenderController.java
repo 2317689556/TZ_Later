a39a0e8c3dabbc02206d4802056a386124406934
010413 <2509764291@qq.com>
2019-12-09 15:59:52 +0800

    厂家页面8

---
+++ src/main/java/spring/controller/VenderController.java
@@ -0,0 +1,25 @@
+package spring.controller;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import spring.pojo.Agency;
+import spring.service.VenderService;
+import javax.annotation.Resource;
+
+//库
+@Controller
+@RequestMapping("/Vender")
+public class VenderController {
+    @Resource
+    VenderService venderService;
+
+    @RequestMapping("findVender")
+    @ResponseBody
+    public PageInfo<Agency> findVender(Page page){
+        PageInfo<Agency> list = venderService.findVender(page);
+        return list;
+    }
+}
